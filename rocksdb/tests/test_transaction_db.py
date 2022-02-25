6e639ddbfacdd5cb1c54dc574032c1d0d12ccca9
FdelMazo <FdelMazo@fi.uba.ar>
2022-02-25 13:29:11 -0300

    Polish test file (documentation and format)

--- rocksdb/tests/test_transaction_db.py
+++ rocksdb/tests/test_transaction_db.py
@@ -12,6 +12,8 @@ from rocksdb.merge_operators import UintAddOperator, StringAppendOperator
 from .test_db import TestHelper, TestDB
 
 class TestTransactionDB(TestDB):
+    """Re-run TestDB with the TransactionDB object"""
+
     def setUp(self):
         TestHelper.setUp(self)
         opts = rocksdb.Options(create_if_missing=True)
@@ -25,7 +27,9 @@ class TestTransactionDB(TestDB):
         expected = "Options object is already used by another DB"
         tdb_opts = rocksdb.TransactionDBOptions()
         with self.assertRaisesRegex(Exception, expected):
-            rocksdb.TransactionDB(os.path.join(self.db_loc, "test2"), self.db.options, tdb_opts=tdb_opts)
+            rocksdb.TransactionDB(os.path.join(self.db_loc, "test2"),
+                                  self.db.options,
+                                  tdb_opts=tdb_opts)
 
     def test_transaction_options_used_twice(self):
         expected = "Transaction Options object is already used by another DB"
