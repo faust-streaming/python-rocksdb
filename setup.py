dff95439be07f3f1ad7ebaf39fac74e8d4eea5eb
Alexander Regueiro <alexreg@gmail.com>
2022-03-08 16:55:55 +0000

    Use pkgconfig in `setup.py` script.

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import os
 import platform
-import setuptools
 import sys
 
+import pkgconfig
+from Cython.Build import cythonize
+from pkgconfig import PackageNotFoundError
+from setuptools import Extension, setup
 
 extra_compile_args = [
     '-std=c++11',
@@ -21,22 +25,31 @@ if platform.system() == 'Darwin':
 if sys.version_info < (3 , 0):
     raise Exception("python-rocksdb requires Python 3.x")
 
-setuptools.setup(
-    ext_modules=[
-        setuptools.Extension(
-            'rocksdb._rocksdb',
-            [
-                'rocksdb/_rocksdb.pyx',
-            ],
-            extra_compile_args=extra_compile_args,
-            language='c++',
-            libraries=[
-                'rocksdb',
-                'snappy',
-                'bz2',
-                'z',
-                'lz4',
-            ],
-        ),
+rocksdb_extension = Extension(
+    'rocksdb._rocksdb',
+    [
+        'rocksdb/_rocksdb.pyx',
     ],
+    extra_compile_args=extra_compile_args,
+    language='c++',
+    libraries=['rocksdb'],
+)
+
+try:
+    pkgconfig.configure_extension(rocksdb_extension, "rocksdb")
+except PackageNotFoundError:
+    include_path = os.environ.get("INCLUDE_PATH")
+    library_path = os.environ.get("LIBRARY_PATH")
+
+    rocksdb_extension.include_dirs += include_path.split(os.pathsep) if include_path else []
+    rocksdb_extension.library_dirs += library_path.split(os.pathsep) if library_path else []
+    rocksdb_extension.libraries += [
+        'snappy',
+        'bz2',
+        'z',
+        'lz4',
+    ]
+
+setup(
+    ext_modules=cythonize([rocksdb_extension]),
 )
